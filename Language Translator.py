9dcc9b570c679722704183a1605a2b681279ddf4
Prathamesh2412 <128672646+Prathamesh2412@users.noreply.github.com>
2023-11-13 18:58:47 +0530

    Language Translator.py

--- Language Translator.py
+++ Language Translator.py
@@ -27,27 +27,27 @@ def Translate():
 Label(Screen, text="WELCOME TO THE TRANSLATER").grid(row=0, column=1)
 
 # Choice for input language
-InputLanguageChoiceMenu = OptionMenu(Screen, InputLanguageChoice, *LanguageChoices, width=15, relief=GROOVE)
+InputLanguageChoiceMenu = OptionMenu(Screen, InputLanguageChoice, *LanguageChoices)
 Label(Screen, text="Enter a language").grid(row=2, column=1)
 InputLanguageChoiceMenu.grid(row=3, column=1)
 
 # Choice in which the language is to be translated
-NewLanguageChoiceMenu = OptionMenu(Screen, TranslateLanguageChoice, *LanguageChoices, width=15, relief=GROOVE)
+NewLanguageChoiceMenu = OptionMenu(Screen, TranslateLanguageChoice, *LanguageChoices)
 Label(Screen, text="Translated Language").grid(row=2, column=2)
 NewLanguageChoiceMenu.grid(row=3, column=2)
 
 Label(Screen, text="Enter Text").grid(row=4, column=0)
 TextVar = StringVar()
-TextBox = Entry(Screen, textvariable=TextVar, width=50).grid(row=4, column=1)
+TextBox = Entry(Screen, textvariable=TextVar).grid(row=4, column=1)
 
 Label(Screen, text="Output Text").grid(row=4, column=2)
 OutputVar = StringVar()
-TextBox = Entry(Screen, textvariable=OutputVar, width=50).grid(row=4, column=3)
+TextBox = Entry(Screen, textvariable=OutputVar).grid(row=4, column=3)
 
 Label(Screen, text="Tip: 1. Write input text in CAPITALS to get Output in ENGLISH").grid(row=6, column=1)
 Label(Screen, text="   Tip: 2. Write input in SMALLS to get Output in DESIRED LANGUAGE").grid(row=7, column=1)
 Label(Screen, text="\nCreated by Rohan, Rishikesh, Prathamesh, Amar ").grid(row=8, column=1)
 
 # Button for calling function
-B = Button(Screen, text="Translate", command=Translate, relief=GROOVE, bg='#4CAF50', fg='white').grid(row=5, column=1, columnspan=3)
+B = Button(Screen, text="Translate", command=Translate, relief=GROOVE).grid(row=5, column=1, columnspan=3)
 mainloop()
